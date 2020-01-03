e2d30529354b1bf11bfac85ba74acb915718dda5
exitsky <exitsky@gmail.com>
2020-01-03 16:54:52 +0900

    Add Material UI Design Modal

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { post } from 'axios';
-//import axios from 'axios';
-
-/*
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-*/
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+    hidden: {
+      display: 'none'
+    }
+  });
+
 class CustomerAdd extends React.Component {
 
     constructor(props) {
@@ -20,7 +24,8 @@ class CustomerAdd extends React.Component {
         birthday: '',
         gender: '',
         job: '',
-        fileName: ''
+        fileName: '',
+        open: false
       }
       /* this.handleFormSubmit = this.handleFormSubmit.bind(this)
       this.handleFileChange = this.handleFileChange.bind(this)
@@ -28,6 +33,24 @@ class CustomerAdd extends React.Component {
       this.addCustomer = this.addCustomer.bind(this)
       this.handleClickOpen = this.handleClickOpen.bind(this)
       this.handleClose = this.handleClose.bind(this);   */    
+    }
+
+    handleClickOpen = () => {
+        this.setState({
+          open: true
+        });
+      }
+    
+    handleClose = () => {
+        this.setState({
+          file: null,
+          userName: '',
+          birthday: '',
+          gender: '',
+          job: '',
+          fileName: '',
+          open: false
+        })
     }
     //
     handleFormSummit = (e) => {
@@ -45,7 +68,8 @@ class CustomerAdd extends React.Component {
             birthday: '',
             gender: '',
             job: '',
-            fileName: ''
+            fileName: '',
+            open: false
         })
         //window.location.reload();
       
@@ -106,7 +130,33 @@ class CustomerAdd extends React.Component {
     } 
 
     render() {
+        const { classes } = this.props;
         return (
+            <div>
+                <Button variant="contained" color="primary" onClick={this.handleClickOpen}>고객 추가하기</Button>
+                <Dialog open={this.state.open} onClose={this.handleClose}>
+                    <DialogTitle>고객 추가</DialogTitle>
+                    <DialogContent>
+                        <input className={classes.hidden} accept="image/*" id="raised-button-file" type="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/>
+                        <label htmlFor="raised-button-file">
+                            <Button variant="contained" color="primary" component="span" name="file">
+                                {this.state.fileName === "" ? "프로필 이미지 선택" : this.state.fileName}
+                            </Button>
+
+                        </label>
+                        <br/>
+                        <TextField label="이름" type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange} /><br/>
+                        <TextField type="생년월일" name="birthday" value={this.state.birthday} onChange={this.handleValueChange} /><br/>
+                        <TextField type="성별" name="gender" value={this.state.gender} onChange={this.handleValueChange} /><br/>
+                        <TextField type="직업" name="job" value={this.state.job} onChange={this.handleValueChange} /><br/>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant="contained" color="primary" onClick={this.handleFormSummit}>추가</Button>
+                        <Button variant="outlined" color="primary" onClick={this.handleClose}>닫기</Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+            /*
             <form onSubmit={this.handleFormSummit}>
                 <h1>고객추가</h1>
                 프로필 이미지: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>
@@ -116,8 +166,9 @@ class CustomerAdd extends React.Component {
                 직업 : <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange} /><br/>
                 <button type="submit">추가하기</button>
             </form>
+            */
         )
     }
 }
 
-export default CustomerAdd;
+export default withStyles(styles)(CustomerAdd);
